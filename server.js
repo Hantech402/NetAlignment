f51e6769b91a9776716e60729052698ff8f64f4c
Victor Zamfir <zamfir.victor@gmail.com>
2016-12-20 16:09:33 +0200

    fix lint errors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import Confidence from 'confidence';
 import manifestConfig from './serverManifest.json';
 import createServer from './src/createServer';
 
-process.once('uncaughtException', console.log);
+process.once('uncaughtException', console.log); // eslint-disable-line no-console
 
 const store = new Confidence.Store(manifestConfig);
 createServer(store);
