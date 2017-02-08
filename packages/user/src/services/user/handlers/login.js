437e00ea396955cd6fa6b5d64ec1a5c3efcd3a25
Victor Zamfir <zamfir.victor@gmail.com>
2017-02-09 00:03:23 +0200

    users can't login when their account is deactivated

--- packages/user/src/services/user/handlers/login.js
+++ packages/user/src/services/user/handlers/login.js
@@ -32,7 +32,7 @@ const handler = async ({ username, UserEntity, AccountEntity, next, params }) =>
     throw Boom.badRequest('Account is not active!');
   }
 
-  if (!skipDeactivationCheck && !account.isDeactivated) {
+  if (!skipDeactivationCheck && account.isDeactivated) {
     throw Boom.badRequest(`Your account is deactivated! Reason - "${account.deactivationReason}".`);
   }
 
