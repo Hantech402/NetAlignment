bf45fc258824cd5d37b58bf054893f10a6d1c1db
Victor Zamfir <zamfir.victor@gmail.com>
2017-02-21 15:29:07 +0200

    improved octobus service template

--- packages/core/docs/serviceTemplate.js
+++ packages/core/docs/serviceTemplate.js
@@ -6,13 +6,11 @@ const { withLookups, withHandler, withSchema } = decorators;
 const schema = Joi.object().keys({
 }).required();
 
-const handler = async ({ params, next }) => { // eslint-disable-line arrow-body-style
-  return next(params);
-};
-
 export default applyDecorators([
   withSchema(schema),
   withLookups({
   }),
   withHandler,
-], handler);
+], async ({ params, next }) => { // eslint-disable-line arrow-body-style
+  return next(params);
+});
