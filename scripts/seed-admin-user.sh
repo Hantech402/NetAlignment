3f31d1cd50b3e08826d52e66b04d28ad7518742b
David Hasenjaeger <david@wirestorm.us>
2017-06-22 20:44:03 -0700

    modified admin creation script

--- scripts/seed-admin-user.sh
+++ scripts/seed-admin-user.sh
@@ -1,1 +1,2 @@
-mongoimport --db net-alignment --host --collection User --file ./seed-admin-user.json
+mongoimport --db net-alignment --host 127.0.0.1:27017 --collection User --file ./seed-admin-user.json
+mongoimport --db net-alignment --host 127.0.0.1:27017 --collection Account --file ./seed-admin-account.json
