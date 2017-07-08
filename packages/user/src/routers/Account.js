16f573d32663cc1a628f6622a84b864a47964c9e
doflefou <senegal2002@gmail.com>
2017-07-07 20:48:09 -0700

    Implement find broker

--- packages/user/src/routers/Account.js
+++ packages/user/src/routers/Account.js
@@ -2,6 +2,8 @@ import Joi from 'joi';
 import AccountRouter from 'makeen-user/build/routers/Account';
 import { route } from 'makeen-router';
 import { ObjectID as objectId } from 'mongodb';
+import Boom from 'boom';
+import { pick } from 'lodash';
 
 class NetAlignAccountRouter extends AccountRouter {
   constructor(
@@ -47,6 +49,65 @@ class NetAlignAccountRouter extends AccountRouter {
       _id: accountId,
       reason,
     });
+  }
+
+  @route.get({
+    path: '/find-broker',
+    config: {
+      auth: false,
+      validate: {
+        query: {
+          employeeEmail: Joi.string().email(),
+          licenseNumber: Joi.string(),
+        },
+      },
+      description: 'Finder broker via email or license number',
+    },
+  })
+  async findBroker(request) {
+    const { licenseNumber, employeeEmail } = request.query;
+
+    try {
+      const query = {
+        isActive: true,
+        isDeactivated: false,
+        isConfirmed: true,
+      };
+
+      if (licenseNumber) {
+        query.licenseNr = licenseNumber;
+      }
+
+      if (employeeEmail) {
+        query.loanOfficersEmails = employeeEmail;
+      }
+
+      const account = await this.Account.AccountRepository.findOne({ query });
+
+      if (!account) {
+        return Boom.notFound('Unable to find account!');
+      }
+
+      const user = await this.User.UserRepository.findOne({
+        query: {
+          accountId: account._id,
+          role: 'broker',
+          isAccountOwner: true,
+        },
+      });
+
+      if (!user) {
+        return Boom.notFound('Unable to find account!');
+      }
+
+      return {
+        accountId: account._id,
+        userId: user._id,
+        ...pick(user, ['firstName', 'middleName', 'lastName']),
+      };
+    } catch (err) {
+      return Boom.wrap(err);
+    }
   }
 }
 
