c9a9c8d90d8dbe9a7b310fc5f80ea45ab68eeb73
 Aleksey Panchenko <alekseypanchenko@MacBook-Pro-Aleksey.local>
2018-02-07 15:12:50 +0200

    Add route DELETE /loans/applications/deleteOne

--- packages/loan/src/routes/application.js
+++ packages/loan/src/routes/application.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { ObjectID as objectId } from 'mongodb';
 import Boom from 'boom';
+import Celebrate from 'celebrate';
+import Joi from 'joi';
 
 export const applicationRouter = config => {
   const {
@@ -56,6 +58,26 @@ export const applicationRouter = config => {
           update: { $set: req.body },
         });
 
+        res.sendStatus(200);
+      } catch (err) {
+        next(err);
+      }
+    },
+  );
+
+  router.delete(
+    '/deleteOne',
+    Celebrate({ body: Joi.object().keys({
+      query: Joi.object().required(),
+    }).required() }),
+    async (req, res, next) => {
+      try {
+        const accountId = objectId(req.user.accountId);
+        const query = { ...req.body.query, accountId };
+        const loanAppExist = await LoanApplicationRepository.count({ query });
+        if (!loanAppExist) throw Boom.notFound('Unable to find loan application');
+
+        await LoanApplicationRepository.deleteOne({ query });
         res.sendStatus(200);
       } catch (err) {
         next(err);
